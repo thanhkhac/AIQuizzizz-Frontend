1db9530d61129747e83681060189d41e42e669eb
Duong Nguyen <91860852+Watssupz@users.noreply.github.com>
2025-08-24 23:53:02 +0700

    Update Billing
    1. Update UI & pagination

--- src/views/user/settings/billing.vue
+++ src/views/user/settings/billing.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onActivated, onMounted, ref, watch } from "vue";
+import { onActivated, onMounted, reactive, ref, watch } from "vue";
 import { useI18n } from "vue-i18n";
 import Input from "@/shared/components/Common/Input.vue";
 import { message } from "ant-design-vue";
@@ -7,8 +7,11 @@ import ApiPlan from "../../../../src/api/ApiPlan";
 import type HistoryPayment from "../../../../src/models/request/plan/historyPayment";
 import type PaymentHistory from "../../../../src/models/response/plan/paymentHistory";
 import { useAuthStore } from "@/stores/AuthStore";
+import { useRoute } from "vue-router";
+import { data } from "jquery";
 
 const { t } = useI18n();
+const route = useRoute();
 const authStore = useAuthStore();
 const user_info = authStore.getUserInfo();
 const modal_qr_open = ref(false);
@@ -49,8 +52,16 @@ watch(
 
 const getHistoryPayment = async () => {
     try {
-        let result = await ApiPlan.HistoryPayment(historyPaymentPayload.value);
+        let result = await ApiPlan.HistoryPayment(pageParams as HistoryPayment);
         dataSource.value = result.data.data.items;
+
+        if (result.data.success) {
+            let resultData = result.data.data;
+            dataSource.value = resultData.items;
+            pageParams.pageNumber = resultData.pageNumber;
+            pageParams.pageSize = resultData.pageSize;
+            pageParams.totalCount = resultData.totalCount;
+        }
     } catch (err) {
         console.log(err);
     }
@@ -91,7 +102,9 @@ const columns = [
     {
         title: "No",
         dataIndex: "no",
-        customRender: ({ index }: { index: number }) => index + 1,
+        customRender: ({ index }: { index: number }) => {
+            return (pageParams.pageNumber - 1) * pageParams.pageSize + index + 1;
+        },
         width: 50,
         align: "center",
     },
@@ -127,6 +140,21 @@ const columns = [
     },
 ];
 
+const pageParams = reactive({
+    pageNumber: route.query.pageNumber || 1,
+    pageSize: route.query.pageSize || 5,
+    totalCount: 0,
+    statusFilter: false,
+});
+
+//change when page change (pageParams)
+const onPaginationChange = (page: any, pageSize: any) => {
+    pageParams.pageNumber = page;
+    pageParams.pageSize = pageSize;
+    pageParams.statusFilter = true;
+    getHistoryPayment();
+};
+
 onMounted(() => {
     getHistoryPayment();
 });
@@ -140,12 +168,19 @@ onMounted(() => {
                     <span>{{ $t("settings.billing.sub_title") }}</span>
                 </div>
                 <div class="w-50 justify-content-center m-2">
-                    <span class="text-end">AIQuizz Points: {{ user_info.balance }}</span>
+                    <span class="text-end"
+                        >AIQuizz Points: {{ formatCurrency(user_info.balance) }}</span
+                    >
                 </div>
             </div>
 
             <div class="w-100">
-                <a-table :data-source="dataSource" :columns="columns" row-key="id">
+                <a-table
+                    :data-source="dataSource"
+                    :columns="columns"
+                    row-key="id"
+                    :pagination="false"
+                >
                     <template #bodyCell="{ column, text }">
                         <template v-if="column.key === 'date'"> {{ formatDate(text) }}</template>
 
@@ -158,31 +193,48 @@ onMounted(() => {
                     </template>
                 </a-table>
             </div>
+            <div class="pagination-container" style="margin-top: 10px">
+                <a-pagination
+                    @change="onPaginationChange"
+                    v-model:current="pageParams.pageNumber"
+                    :total="pageParams.totalCount"
+                    :pageSize="pageParams.pageSize"
+                    :show-total="
+                        (total: any, range: any) =>
+                            `${range[0]}-${range[1]} of ${total} ${t('settings.billing.transaction_count')}`
+                    "
+                    show-size-changer
+                    class="crud-layout-pagination"
+                    :locale="{
+                        items_per_page: t('class_index.other.pages'),
+                    }"
+                ></a-pagination>
+            </div>
         </div>
 
         <div class="content-item">
             <div class="content-item-title">
                 <div class="w-50">
-                    <span>{{ $t("settings.billing.title") }}</span>
-                    <span>{{ $t("settings.billing.sub_title") }}</span>
+                    <span>{{ $t("settings.billing.deposit.title") }}</span>
+                    <span>{{ $t("settings.billing.deposit.content") }}</span>
                 </div>
             </div>
-            <div class="d-flex">
-                <div class="content-item-title w-50 d-flex flex-column">
-                    <div class="content-item w-75">
+            <div class="d-flex justify-content-center">
+                <div class="content-item-title w-50 d-flex flex-column align-items-center">
+                    <div class="content-item">
                         <span>{{ t("settings.billing.bank.name") }}</span>
                         <span>Nguyen Khac Thanh</span>
                     </div>
-                    <div class="content-item w-75">
+                    <div class="content-item">
                         <span>{{ t("settings.billing.bank.number") }}</span>
                         <span>4271033443</span>
                     </div>
-                    <div class="content-item w-75">
+                    <div class="content-item">
                         <span>{{ t("settings.billing.bank.bank_name") }}</span>
                         <span>BIDV</span>
                     </div>
                 </div>
-                <div class="w-50">
+                <div class="w-50" style="justify-items: center">
                     <div class="content-item d-flex flex-column gap-2">
                         <div class="d-flex justify-content-center">
                             <i class="me-2 bx bx-qr" style="font-size: 24px"></i>
@@ -226,7 +278,7 @@ onMounted(() => {
                                 <i class="me-2 bx bx-info-circle" style="font-size: 24px"></i>
                                 <span class="fw-bold">{{ t("settings.billing.noti.title") }}</span>
                             </div>
-                            <span>• {{ t("settings.billing.noti.content1") }}: 5000 VNĐ</span>
+                            <span>• {{ t("settings.billing.noti.content1") }}: 5,000 VNĐ</span>
                             <span>• {{ t("settings.billing.noti.content2") }}</span>
                         </div>
                     </div>
@@ -317,7 +369,7 @@ onMounted(() => {
     border: 1px solid rgba(255, 215, 0, 1);
 }
 .noti-content {
-    width: calc(100% / 2);
+    width: calc(100% - 20px);
     margin: 8px;
     margin-bottom: 0px;
     padding: 10px;
